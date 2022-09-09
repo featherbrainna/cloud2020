0a9a3ebc5b5e06b159639eb254e401f8d21f0a73
Wangzhongyi <578996401@qq.com>
2022-09-10 00:05:31 +0800

    first commit(wzy)

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 王忠义
+ * @version 1.0
+ * @date: 2022/9/9 22:04
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+
+    {
+        return configInfo;
+    }
+}
